f1d9729e299c72d2b68e0507ef58a07ed11eb646
ItzikDevio <itzikdevio@gmail.com>
2022-02-18 23:28:57 +0200

    Changed the cron time to 6:30

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,6 @@ const functions = require("firebase-functions");
 const controller = require("./controller/index.js");
 
 exports.weatherScheduler = functions.pubsub
-  .schedule("30 8 * * *")
+  .schedule("5 6 * * *")
   .timeZone("Asia/Jerusalem")
   .onRun((context) => controller.sendWeatherGroupMessage());

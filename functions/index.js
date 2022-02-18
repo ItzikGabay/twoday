0b156ed393291b409ad3aac3db799cf8f187f3e2
ItzikDevio <itzikdevio@gmail.com>
2022-02-18 23:27:53 +0200

    Changed the cron timezones to jerusalem

--- functions/index.js
+++ functions/index.js
@@ -2,5 +2,6 @@ const functions = require("firebase-functions");
 const controller = require("./controller/index.js");
 
 exports.weatherScheduler = functions.pubsub
-  .schedule("every day 06:05")
+  .schedule("30 8 * * *")
+  .timeZone("Asia/Jerusalem")
   .onRun((context) => controller.sendWeatherGroupMessage());
